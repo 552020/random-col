febccfcded831dcc71f80d3c6fb408b3065a8b3b
552020 <stefanolombardo@posteo.de>
2021-09-05 23:23:27 +0200

    readme and online

---
+++ README.md
@@ -0,0 +1,7 @@
+# random-col
+
+Some code playing with event propagation
+
+## online
+
+[random-col-online](https://random-col.netlify.app/)
--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const randomColor = () =>
 
 document.querySelectorAll(".item").forEach((el) =>
   el.addEventListener("click", function (e) {
-    console.log("hallo!");
+    console.log("hallo-item", e.target, e.currentTarget);
+
     this.style.backgroundColor = randomColor();
   })
 );
@@ -17,10 +18,11 @@ document.querySelectorAll(".item").forEach((el) =>
 
 document.querySelector(".container").addEventListener("click", function (e) {
   console.log("hallo-container");
+  console.log(e.target, e.currentTarget);
   this.style.backgroundColor = randomColor();
 });
 
 document.querySelector(".h1").addEventListener("click", function () {
-  console.log("hallo-container");
+  console.log("hallo-h1");
   this.style.backgroundColor = randomColor();
 });
